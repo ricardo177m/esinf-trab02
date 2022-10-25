58d853b05fe734507a3a6b499f81b0c7f3b06958
Tomás Russo <tomas.russo4@gmail.com>
2022-10-25 17:40:51 +0100

    started AVL class

---
+++ src/main/java/isep/esinf/utils/AVL.java
@@ -0,0 +1,39 @@
+package isep.esinf.utils;
+
+/**
+ * AVL class.
+ * 
+ * @author Carlos Lopes <XX@isep.ipp.pt>
+ * @author Ricardo Moreira <XX@isep.ipp.pt>
+ * @author Tomás Russo <1211288@isep.ipp.pt>
+ * @param <E>
+ */
+public class AVL<E extends Comparable<E>> extends BST<E> implements AVLInterface<E> {
+  public int balanceFactor(Node<E> node) {
+    return 0;
+  }
+
+  public Node<E> rightRotation(Node<E> node) {
+    return null;
+  }
+
+  public Node<E> leftRotation(Node<E> node) {
+    return null;
+  }
+
+  public Node<E> twoRotations(Node<E> node) {
+    return null;
+  }
+
+  public Node<E> balanceNode(Node<E> node) {
+    return null;
+  }
+
+  public void insert(E element) {
+
+  }
+
+  public void remove(E element) {
+
+  }
+}
