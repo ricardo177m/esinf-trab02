91f980768d8baef49a68c07245896eef6d11ec79
André Barros <andrebarros0426@gmail.com>
2022-10-25 23:03:47 +0100

    tests: added find test twoDtree

--- src/test/java/isep/esinf/utils/TwoDTreeTest.java
+++ src/test/java/isep/esinf/utils/TwoDTreeTest.java
@@ -9,35 +9,44 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TwoDTreeTest {
-  Point2D.Double[] arr = new Point2D.Double[6];
-  {
-    arr[0] = new Point2D.Double(30, 40);
-    arr[1] = new Point2D.Double(5, 25);
-    arr[2] = new Point2D.Double(10, 12);
-    arr[3] = new Point2D.Double(70, 70);
-    arr[4] = new Point2D.Double(50, 30);
-    arr[5] = new Point2D.Double(35, 45);
-  }
+	Point2D.Double[] arr = new Point2D.Double[6];
+	{
+		arr[0] = new Point2D.Double(30, 40);
+		arr[1] = new Point2D.Double(5, 25);
+		arr[2] = new Point2D.Double(10, 12);
+		arr[3] = new Point2D.Double(70, 70);
+		arr[4] = new Point2D.Double(50, 30);
+		arr[5] = new Point2D.Double(35, 45);
+	}
 
-  private static TwoDTree<Double> tree;
+	private static TwoDTree<Double> tree;
 
-  @BeforeEach
-  public void setUp() {
-    tree = new TwoDTree<>();
+	@BeforeEach
+	public void setUp() {
+		tree = new TwoDTree<>();
 
-    tree.insert(30.40, arr[0].getX(), arr[0].getY());
-    tree.insert(5.25, arr[1].getX(), arr[1].getY());
-    tree.insert(10.12, arr[2].getX(), arr[2].getY());
-    tree.insert(70.70, arr[3].getX(), arr[3].getY());
-    tree.insert(50.30, arr[4].getX(), arr[4].getY());
-    tree.insert(35.45, arr[5].getX(), arr[5].getY());
-  }
+		tree.insert(30.40, arr[0].getX(), arr[0].getY());
+		tree.insert(5.25, arr[1].getX(), arr[1].getY());
+		tree.insert(10.12, arr[2].getX(), arr[2].getY());
+		tree.insert(70.70, arr[3].getX(), arr[3].getY());
+		tree.insert(50.30, arr[4].getX(), arr[4].getY());
+		tree.insert(35.45, arr[5].getX(), arr[5].getY());
+	}
 
-  @Test
-  public void testInsertionTwoDTree() {
-    Iterable<Double> expected = List.of(10.12, 5.25, 30.40, 35.45, 50.30, 70.70);
-    Iterable<Double> actual = tree.inOrder();
+	@Test
+	public void testInsertionTwoDTree() {
+		Iterable<Double> expected = List.of(10.12, 5.25, 30.40, 35.45, 50.30, 70.70);
+		Iterable<Double> actual = tree.inOrder();
 
-    assertEquals(expected, actual);
-  }
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testFindTwoDTree() {
+		assertEquals(tree.find(tree.root, 30.40).getElement(), 30.40);
+		assertEquals(tree.find(tree.root, 30.40).getLeft().getElement(), 5.25);
+		assertEquals(tree.find(tree.root, 30.40).getRight().getElement(), 70.70);
+
+	}
+
 }
