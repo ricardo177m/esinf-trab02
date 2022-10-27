fc40cf6627a02a691e110abe95fedb526fc8a590
Ricardo M <rmoreiradev@gmail.com>
2022-10-27 21:15:19 +0100

    feat(load-data): beginning of load data use case
    Former-commit-id: 5085bc21257f95e4f790ca25002057a20473c148

--- src/main/java/isep/esinf/usecase/LoadData.java
+++ src/main/java/isep/esinf/usecase/LoadData.java
@@ -1,6 +1,9 @@
 package isep.esinf.usecase;
 
+import java.util.List;
+import java.util.Map;
 import isep.esinf.utils.AVL;
+import isep.esinf.utils.CSVReader;
 
 /**
  * Alínea 1.
@@ -8,7 +11,9 @@ import isep.esinf.utils.AVL;
  * Load FAOSTAT data from a file into an AVL tree.
  */
 public class LoadData {
-  public static void execute(String[] args) {
-    // TODO
+  public static void execute(List<? extends Map<String, String>> data) {
+    // import data from a csv file into an AVL
+    AVL<> avl = new AVL<>();
+
   }
 }
