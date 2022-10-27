1c0bbce08243575c2e17a6cb7208ec86164585b7
Ricardo <rmoreiradev@gmail.com>
2022-10-27 02:29:02 +0100

    feat: load data use case

---
+++ src/main/java/isep/esinf/usecase/LoadData.java
@@ -0,0 +1,14 @@
+package isep.esinf.usecase;
+
+import isep.esinf.utils.AVL;
+
+/**
+ * Alínea 1.
+ *
+ * Load FAOSTAT data from a file into an AVL tree.
+ */
+public class LoadData {
+  public static void execute(String[] args) {
+    // TODO
+  }
+}
