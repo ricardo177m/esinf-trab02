5c853fded448043f2a59ffd19c91a1c3e153cfed
Ricardo <rmoreiradev@gmail.com>
2022-10-27 01:34:04 +0100

    implemented single & double rotation algorithms

--- src/main/java/isep/esinf/utils/AVL.java
+++ src/main/java/isep/esinf/utils/AVL.java
@@ -2,7 +2,7 @@ package isep.esinf.utils;
 
 /**
  * AVL class.
- * 
+ *
  * @author Carlos Lopes <XX@isep.ipp.pt>
  * @author Ricardo Moreira <XX@isep.ipp.pt>
  * @author Tomás Russo <1211288@isep.ipp.pt>
@@ -11,9 +11,9 @@ package isep.esinf.utils;
 public class AVL<E extends Comparable<E>> extends BST<E> implements AVLInterface<E> {
   /**
    * Returns the balance factor of the given node.
-   * 
+   *
    * @param node a valid Node within the tree
-   * 
+   *
    * @return balance factor of the given node
    */
   public int balanceFactor(Node<E> node) {
@@ -24,15 +24,28 @@ public class AVL<E extends Comparable<E>> extends BST<E> implements AVLInterface
   }
 
   private Node<E> rightRotation(Node<E> node) {
-    return null;
+    Node<E> leftChild = node.getLeft();
+    node.setLeft(leftChild.getRight());
+    leftChild.setRight(node);
+    return leftChild; // left child is now the root of this subtree
   }
 
   private Node<E> leftRotation(Node<E> node) {
-    return null;
+    Node<E> rightChild = node.getRight();
+    node.setRight(rightChild.getLeft());
+    rightChild.setLeft(node);
+    return rightChild; // right child is now the root of this subtree
   }
 
   private Node<E> twoRotations(Node<E> node) {
-    return null;
+    if (balanceFactor(node) < 0) {
+      node.setLeft(leftRotation(node.getLeft()));
+      node = rightRotation(node);
+    } else {
+      node.setRight(rightRotation(node.getRight()));
+      node = leftRotation(node);
+    }
+    return node;
   }
 
   public Node<E> balanceNode(Node<E> node) {
