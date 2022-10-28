677b9050bd5743e89c8d5dfbf3269b644e422fca
Tomás Lopes <tomas050302@gmail.com>
2022-10-28 12:49:41 +0100

    chore: added usecase files

---
+++ src/main/java/isep/esinf/usecase/LoadData.java
@@ -0,0 +1,12 @@
+package isep.esinf.usecase;
+
+/**
+ * Alínea 1
+ * 
+ * @author Ricardo Moreira
+ */
+public class LoadData {
+  public void execute() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
