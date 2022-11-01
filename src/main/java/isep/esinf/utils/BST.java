0f3126c7a0677be1e76d73127282239540955b51
Tomás Russo <tomas.russo4@gmail.com>
2022-11-01 16:08:21 +0000

    feat: added biggestElement method to BST

--- src/main/java/isep/esinf/utils/BST.java
+++ src/main/java/isep/esinf/utils/BST.java
@@ -46,8 +46,10 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
    * @param element the element to find
    * @return the Node that contains the Element, or null otherwise
    *
-   *         This method despite not being essential is very useful. It is written here in order to
-   *         be used by this class and its subclasses avoiding recoding. So its access level is
+   *         This method despite not being essential is very useful. It is written
+   *         here in order to
+   *         be used by this class and its subclasses avoiding recoding. So its
+   *         access level is
    *         protected
    */
   protected Node<E> find(Node<E> node, E element) {
@@ -96,7 +98,8 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
   }
 
   /**
-   * Removes an element from the tree maintaining its consistency as a Binary Search Tree.
+   * Removes an element from the tree maintaining its consistency as a Binary
+   * Search Tree.
    */
   public void remove(E element) {
     root = remove(element, root());
@@ -187,6 +190,24 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
     return smallestElement(node.getLeft());
   }
 
+  /**
+   * Returns the biggest element within the tree.
+   *
+   * @return the biggest element within the tree
+   */
+  public E biggestElement() {
+    if (root == null)
+      return null;
+    return biggestElement(root);
+  }
+
+  protected E biggestElement(Node<E> node) {
+    if (node.getRight() == null)
+      return node.getElement();
+
+    return biggestElement(node.getRight());
+  }
+
   /*
    * Returns an iterable collection of elements of the tree, reported in in-order.
    *
@@ -200,10 +221,11 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
   }
 
   /**
-   * Adds elements of the subtree rooted at Node node to the given snapshot using an in-order
+   * Adds elements of the subtree rooted at Node node to the given snapshot using
+   * an in-order
    * traversal
    *
-   * @param node Node serving as the root of a subtree
+   * @param node     Node serving as the root of a subtree
    * @param snapshot a list to which results are appended
    */
   private void inOrderSubtree(Node<E> node, List<E> snapshot) {
@@ -215,7 +237,8 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
   }
 
   /**
-   * Returns an iterable collection of elements of the tree, reported in pre-order.
+   * Returns an iterable collection of elements of the tree, reported in
+   * pre-order.
    *
    * @return iterable collection of the tree's elements reported in pre-order
    */
@@ -227,10 +250,11 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
   }
 
   /**
-   * Adds elements of the subtree rooted at Node node to the given snapshot using an pre-order
+   * Adds elements of the subtree rooted at Node node to the given snapshot using
+   * an pre-order
    * traversal
    *
-   * @param node Node serving as the root of a subtree
+   * @param node     Node serving as the root of a subtree
    * @param snapshot a list to which results are appended
    */
   private void preOrderSubtree(Node<E> node, List<E> snapshot) {
@@ -242,7 +266,8 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
   }
 
   /**
-   * Returns an iterable collection of elements of the tree, reported in post-order.
+   * Returns an iterable collection of elements of the tree, reported in
+   * post-order.
    *
    * @return iterable collection of the tree's elements reported in post-order
    */
@@ -254,10 +279,11 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E> {
   }
 
   /**
-   * Adds positions of the subtree rooted at Node node to the given snapshot using an post-order
+   * Adds positions of the subtree rooted at Node node to the given snapshot using
+   * an post-order
    * traversal
    *
-   * @param node Node serving as the root of a subtree
+   * @param node     Node serving as the root of a subtree
    * @param snapshot a list to which results are appended
    */
   private void posOrderSubtree(Node<E> node, List<E> snapshot) {
