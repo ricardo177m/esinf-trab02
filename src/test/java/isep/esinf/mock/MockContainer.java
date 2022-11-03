401d2b1da00dbd4566771f8704353268068979c0
Tomás Lopes <tomas050302@gmail.com>
2022-11-03 14:51:57 +0000

    test: tested areas with condition

--- src/test/java/isep/esinf/mock/MockContainer.java
+++ src/test/java/isep/esinf/mock/MockContainer.java
@@ -13,6 +13,116 @@ import isep.esinf.model.comparators.ItemByCode;
 import isep.esinf.model.comparators.ItemByName;
 
 public class MockContainer {
+  public Container mockByCodeSmall() {
+    Container container = new Container();
+
+    Area firstArea = new AreaByCode(100, "200", "Portugal");
+    Area secondArea = new AreaByCode(200, "300", "Spain");
+    Area thirdArea = new AreaByCode(300, "400", "France");
+    Area fourthArea = new AreaByCode(400, "500", "Burundi");
+    Area fifthArea = new AreaByCode(500, "600", "Tuvalu");
+
+    Item firstItem = new ItemByCode(1, "2", "Item 1");
+    Item secondItem = new ItemByCode(2, "3", "Item 2");
+    Item thirdItem = new ItemByCode(3, "4", "Item 3");
+    Item fourthItem = new ItemByCode(4, "5", "Item 4");
+    Item fifthItem = new ItemByCode(5, "6", "Item 5");
+    Item sixthItem = new ItemByCode(6, "7", "Item 6");
+    Item seventhItem = new ItemByCode(7, "8", "Item 7");
+    Item eightItem = new ItemByCode(8, "9", "Item 8");
+    Item ninthItem = new ItemByCode(9, "10", "Item 9");
+
+    Element firstElement = new ElementByCode(1, "Element 1");
+    Element secondElement = new ElementByCode(2, "Element 2");
+    Element thirdElement = new ElementByCode(3, "Element 3");
+    Element fourthElement = new ElementByCode(4, "Element 4");
+
+    Element fifthElement = new ElementByCode(5, "Element 5");
+    Element sixthElement = new ElementByCode(6, "Element 6");
+
+    ProductionData firstProductionData = new ProductionData(1980, 200, "testunit", "testflag");
+    ProductionData secondProductionData = new ProductionData(1981, 300, "testunit", "testflag");
+    ProductionData thirdProductionData = new ProductionData(1982, 400, "testunit", "testflag");
+    ProductionData fourthProductionData = new ProductionData(1983, 500, "testunit", "testflag");
+    ProductionData fifthProductionData = new ProductionData(1984, 600, "testunit", "testflag");
+    ProductionData sixthProductionData = new ProductionData(1985, 700, "testunit", "testflag");
+    ProductionData seventhProductionData = new ProductionData(1986, 800, "testunit", "testflag");
+
+    ProductionData eighthProductionData = new ProductionData(1980, 721.02, "testunit", "testflag");
+
+    firstElement.addProductionData(firstProductionData);
+    firstElement.addProductionData(secondProductionData);
+    firstElement.addProductionData(thirdProductionData);
+    firstElement.addProductionData(eighthProductionData);
+
+    secondElement.addProductionData(firstProductionData);
+    secondElement.addProductionData(fourthProductionData);
+    secondElement.addProductionData(fifthProductionData);
+
+    thirdElement.addProductionData(sixthProductionData);
+    thirdElement.addProductionData(seventhProductionData);
+
+    fourthElement.addProductionData(firstProductionData);
+
+    fifthElement.addProductionData(firstProductionData);
+    fifthElement.addProductionData(eighthProductionData);
+
+    sixthElement.addProductionData(eighthProductionData);
+
+    firstItem.addElement(firstElement);
+    firstItem.addElement(secondElement);
+
+    secondItem.addElement(thirdElement);
+
+    thirdItem.addElement(firstElement);
+    thirdItem.addElement(secondElement);
+
+    fourthItem.addElement(thirdElement);
+    fourthItem.addElement(fourthElement);
+
+    fifthItem.addElement(firstElement);
+    fifthItem.addElement(secondElement);
+
+    sixthItem.addElement(thirdElement);
+
+    seventhItem.addElement(firstElement);
+    seventhItem.addElement(secondElement);
+
+    eightItem.addElement(fourthElement);
+    eightItem.addElement(firstElement);
+    eightItem.addElement(fifthElement);
+
+    ninthItem.addElement(thirdElement);
+
+    firstArea.addItem(firstItem);
+    firstArea.addItem(secondItem);
+    firstArea.addItem(thirdItem);
+    firstArea.addItem(fifthItem);
+
+    secondArea.addItem(secondItem);
+
+    thirdArea.addItem(firstItem);
+    thirdArea.addItem(fifthItem);
+    thirdArea.addItem(sixthItem);
+    thirdArea.addItem(seventhItem);
+
+    fourthArea.addItem(eightItem);
+
+    fourthArea.addItem(ninthItem);
+
+    fifthArea.addItem(firstItem);
+    fifthArea.addItem(eightItem);
+    fifthArea.addItem(ninthItem);
+
+    container.addArea(firstArea);
+    container.addArea(secondArea);
+    container.addArea(thirdArea);
+    container.addArea(fourthArea);
+    container.addArea(fifthArea);
+
+    return container;
+  }
+
   public Container mockByCode() {
     Container container = new Container();
 
@@ -29,7 +139,7 @@ public class MockContainer {
     Item fifthItem = new ItemByCode(5, "6", "Item 5");
     Item sixthItem = new ItemByCode(6, "7", "Item 6");
     Item seventhItem = new ItemByCode(7, "8", "Item 7");
-    Item eigthItem = new ItemByCode(8, "9", "Item 8");
+    Item eightItem = new ItemByCode(8, "9", "Item 8");
     Item ninthItem = new ItemByCode(9, "10", "Item 9");
 
     Element firstElement = new ElementByCode(1, "Element 1");
@@ -92,9 +202,9 @@ public class MockContainer {
     seventhItem.addElement(firstElement);
     seventhItem.addElement(secondElement);
 
-    eigthItem.addElement(fourthElement);
-    eigthItem.addElement(firstElement);
-    eigthItem.addElement(fifthElement);
+    eightItem.addElement(fourthElement);
+    eightItem.addElement(firstElement);
+    eightItem.addElement(fifthElement);
 
     ninthItem.addElement(sixthElement);
 
@@ -114,11 +224,11 @@ public class MockContainer {
     thirdArea.addItem(seventhItem);
 
     fourthArea.addItem(firstItem);
-    fourthArea.addItem(eigthItem);
+    fourthArea.addItem(eightItem);
     fourthArea.addItem(ninthItem);
 
     fifthArea.addItem(firstItem);
-    fifthArea.addItem(eigthItem);
+    fifthArea.addItem(eightItem);
     fifthArea.addItem(ninthItem);
 
     container.addArea(firstArea);
--- src/test/java/isep/esinf/model/ContainerTest.java
+++ src/test/java/isep/esinf/model/ContainerTest.java
@@ -1,8 +1,10 @@
 package isep.esinf.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import isep.esinf.mock.MockContainer;
 import isep.esinf.model.comparators.AreaByCode;
 import isep.esinf.model.comparators.ElementByCode;
@@ -11,7 +13,9 @@ import isep.esinf.model.comparators.ItemByName;
 public class ContainerTest {
   private static Container container;
   private static Area firstArea = new AreaByCode(100, "200", "Portugal");
-  private static Area secondArea = new AreaByCode(200, "300", "Spain");
+  private static Area thirdArea = new AreaByCode(300, "400", "France");
+  private static Area fifthArea = new AreaByCode(500, "600", "Tuvalu");
+
   private static Item firstItem = new ItemByName(1, "2", "Item 1");
   private static Element secondElement = new ElementByCode(2, "Element 2");
   private static final int YEAR = 1980;
@@ -19,14 +23,15 @@ public class ContainerTest {
   @BeforeAll
   public static void setup() {
     // create a container with elements to filter
-    container = (new MockContainer()).mockByCode();
+    container = (new MockContainer()).mockByCodeSmall();
   }
 
   @Test
   public void testGetAreasWithCondition() {
     Container expected = new Container();
     expected.addArea(firstArea);
-    expected.addArea(secondArea);
+    expected.addArea(thirdArea);
+    expected.addArea(fifthArea);
 
     Container actual = container.getAreasWithConditions(firstItem, secondElement, YEAR);
 
