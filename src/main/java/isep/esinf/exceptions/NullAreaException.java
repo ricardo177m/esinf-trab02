7489cd5fb9abdcf6aeab593a5d3f7cf07d47cc6d
carlos <1211277@isep.ipp.pt>
2022-11-03 14:19:15 +0000

    feat: NullAreaException

---
+++ src/main/java/isep/esinf/exceptions/NullAreaException.java
@@ -0,0 +1,10 @@
+package isep.esinf.exceptions;
+
+/**
+ * @author Carlos Lopes
+ */
+public class NullAreaException extends Exception {
+  public NullAreaException(){
+    super("Null Area Exception!");
+  }
+}
