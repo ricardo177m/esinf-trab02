ae981e6ee0e47f3af55a6083cfa12ee0a9febd37
carlos <1211277@isep.ipp.pt>
2022-11-03 14:27:03 +0000

    feat: InvalidTimeIntervalException

---
+++ src/main/java/isep/esinf/exceptions/InvalidTimeIntervalException.java
@@ -0,0 +1,10 @@
+package isep.esinf.exceptions;
+
+/**
+ * @author Carlos Lopes
+ */
+public class InvalidTimeIntervalException extends Exception {
+  public InvalidTimeIntervalException(){
+    super("Invalid Time Interval Exception!");
+  }
+}
